aad9708e8c8c13d91c180c7f01b43633e24124e7
Kris! <42949874+krstphrrr@users.noreply.github.com>
2022-04-22 16:07:13 -0600

    made css flower

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
-import './App.css';
+import './style/style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='circle-container'>
+    <div className="circles --top">
+      <span className='txt'>
+        Land health knowledge systems</span>
+    </div>
+    <div className='circles --right'>
+      <span className='txt'>
+        Climate adaptation for Southwest Agriculture</span>
+
+    </div>
+    <div className='circles --lower'>
+    <span className='txt'>
+      K-12 education and outreach strategies
+      </span>
+    </div>
+
+    <div className='circles --leftlower'>
+    <span className='txt'>
+      Sustainability of agricultural supply chains
+      </span>
+    </div>
+
+    <div className='circles --left'>
+    <span className='txt'>
+      Ecosystem responses to climate and restoration
+      </span>
+    </div>
     </div>
   );
 }
