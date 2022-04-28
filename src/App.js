574a359fae9055cf8252ed0ba594bf5222024390
Kris! <42949874+krstphrrr@users.noreply.github.com>
2022-04-28 16:31:28 -0600

    sass logic

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './style/style.css';
 
 function App() {
   return (
+    <>
     <div className='circle-container'>
-    <div className="circles --top">
+      {/* <div className="circles --top">
+        <span className='txt'>
+          Land health knowledge systems</span>
+      </div>
+
+      <div className='circles --right'>
+        <span className='txt'>
+          Climate adaptation for Southwest Agriculture</span>
+
+      </div>
+      <div className='circles --lower'>
+        <span className='txt'>
+        K-12 education and outreach strategies
+        </span>
+      </div>
+
+      <div className='circles --leftlower'>
+        <span className='txt'>
+          Sustainability of agricultural supply chains
+        </span>
+      </div>
+
+      <div className='circles --left'>
       <span className='txt'>
-        Land health knowledge systems</span>
-    </div>
-    <div className='circles --right'>
-      <span className='txt'>
-        Climate adaptation for Southwest Agriculture</span>
+        Ecosystem responses to climate and restoration
+        </span>
+      </div> */}
 
+      <div className='drops'>
+        <div className='drop'></div>
+        <div className='drop'></div>
+      </div>
     </div>
-    <div className='circles --lower'>
-    <span className='txt'>
-      K-12 education and outreach strategies
-      </span>
-    </div>
-
-    <div className='circles --leftlower'>
-    <span className='txt'>
-      Sustainability of agricultural supply chains
-      </span>
-    </div>
-
-    <div className='circles --left'>
-    <span className='txt'>
-      Ecosystem responses to climate and restoration
-      </span>
-    </div>
-    </div>
-  );
+    </>
+    )
 }
+
 
 export default App;
